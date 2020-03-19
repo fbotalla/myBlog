1e9e77911736cc4220eb11f8df55238170c280f3
fbotalla <botalla.fabrizio@gmail.com>
2020-03-18 14:24:09 -1000

    Milestone 2

--- database.sql
+++ database.sql
@@ -22,6 +22,14 @@ INSERT INTO person (name, last_name,email,username,password) VALUES ('Second', '
 INSERT INTO project(name,languange,image,description) VALUES ('MathProof','Java','mathproof.png','This project was desinged to have children learn simple to complex math by having a fun
 interface in which they could play and learn. This app, written in Java, was a 3 months long effort. Hope you enjoy it!');
 
+INSERT INTO project(name,languange,image,description) VALUES ('BallTalla','HTML/JS/CSS/PHP','balltalla.mp4','This project was the beginning of a business idea that I would like to develop in the future.
+I am passionate about soccer. This future will connect people that like me are passionate about soccer to private trainers that can enhance the student ability and skills to play this sport. This website
+was developped in just over a month. Hope you like it!');
+
+INSERT INTO project(name,languange,image,description) VALUES ('AJAX Registrations','HTML/JS/CSS/AJAX','registrations.mp4','This project was developed for one of my Web Engineering assignments. It simply 
+gathers information from a user, writes it to a file and then it reads the file infomation and displays it on the client without the need for a refresh. This type of programming is often used when buying online.
+Hope you like it!');
+
 
 CREATE USER pro_user WITH PASSWORD 'default';
 GRANT SELECT, INSERT, UPDATE ON person TO pro_user;
